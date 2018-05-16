5fde75272dace2dfb44b5ffd264a7f3a2c790f0f
宋明硕 <songmingshuo@moxiu.net>
2018-05-17 00:10:52 +0800

    支付宝接口

--- src/Config/Alipay.php
+++ src/Config/Alipay.php
@@ -5,6 +5,8 @@ namespace Pay\Config;
  * 支付宝的相关配置
  *
  * date 16/05/18 20:53:59
+ *
+ * @songmingshuo
  */
 
 class Alipay
---
+++ src/Service/Apliy/Transfer.php
@@ -0,0 +1,29 @@
+<?php
+namespace Pay\Service\Apliy;
+
+/*
+ * 支付宝转账接口
+ *
+ * date 16/05/18 23:53:55 
+ *
+ * @songmingshuo
+ */
+
+class Transfer
+{
+    protected function execute()
+    {
+    }
+
+    /*
+     * 进行转账接口
+     *
+     * @return mix
+     */
+    protected function doTransfer()
+    {
+        echo "sdfsdf";
+        exit;
+    }
+}
+?>
